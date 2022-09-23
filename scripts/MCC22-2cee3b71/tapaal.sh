1dc24f3514f7506970d23a8e79ddeb882d3ad816
adam <56338480+adastx@users.noreply.github.com>
2022-09-23 11:25:06 +0200

    Some debug prints and fixed indentation

--- scripts/MCC22-2cee3b71/tapaal.sh
+++ scripts/MCC22-2cee3b71/tapaal.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15017cd5b848fc4af7e448a4d17d4fe36c11a9eef1bc0b7dcd21bb38968a5354
-size 18793
+oid sha256:a52943f5fb84d61549e9c084097cf80f9a7df77e7bdb543a25e57f80f3bc452a
+size 18979
