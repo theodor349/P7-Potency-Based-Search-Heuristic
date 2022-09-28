7683f901c6496924c9bc636bfa55dc12cd5b2e24
adam <56338480+adastx@users.noreply.github.com>
2022-09-28 13:51:51 +0200

    scripts for results->csv->graph

---
+++ csv2cactus.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c3b9388c17f15f81e1fcb2925becc9bfdee2c336318f97c8ea325425d17aaca1
+size 818
--- scripts/MCC22-2cee3b71/tapaal.sh
+++ scripts/MCC22-2cee3b71/tapaal.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d53dcfd6942533c70b555c647e753c2dfaff41b62cb2099cbb592da0c6dfe821
-size 15559
+oid sha256:a9ef07aca1defac5b0d8e49c3ecdd8a30eff9320601e3d0e78952a2fc0601c94
+size 15731
