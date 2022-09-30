9a54b8d6de0cd034802d6791ae1d3eb8940a05ed
Theodor Risager <theododor349@thr349.localdomain>
2022-09-30 08:47:28 +0200

    testRun and testResults.sh

---
+++ test_results_benchkit.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6979e15a67feeec2d4f0f8a9782ecea5ed588f7b400920da458a1eff3c0d15c9
+size 440
--- test_run_benchkit.sh
+++ test_run_benchkit.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edbd4ed30605b38d8c3d243659588ca47bcce8228f2a83c98abb2badc8a29315
-size 528
+oid sha256:e7baaff9b20a956526faaa59e9b356c8f82cd291ea15de5f7cd07908f30602af
+size 516
