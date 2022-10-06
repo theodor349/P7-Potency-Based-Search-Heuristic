b528b2fd6b72bffc850bc6cc83d786eb3f468845
Theodor Risager <theodor349@gmail.com>
2022-10-06 11:42:31 +0200

    Fastests

--- cdpr/ClusterDataParser/Program.cs
+++ cdpr/ClusterDataParser/Program.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1715815b388cfc45bb9d39a834238e9045549896913dc9545f6535c7ec0e1765
-size 183
+oid sha256:82b764f66cd7b273cacd0b4c107ae2555408c3eb6fbcc4ffb1aea03196860d83
+size 434
--- cdpr/DataEngine/StatsEngine.cs
+++ cdpr/DataEngine/StatsEngine.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7dfe64a4e531e23d0b1a7fd147bc83fdca7b0adc28c90c03195fb495bf29c6f4
-size 2204
+oid sha256:793b5c41b2258586ede9529a50632de7b3f709892460d80795be43c94c161224
+size 3141
