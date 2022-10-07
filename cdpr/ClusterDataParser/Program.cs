19d9fd6c6a00b217afe57fb6a95c007713d7f8da
Theodor Risager <theodor349@gmail.com>
2022-10-07 13:33:53 +0200

    Aggregation

--- cdpr/ClusterDataParser/Program.cs
+++ cdpr/ClusterDataParser/Program.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da2d188f941bb8cfe61e9184cc54e50c0b9f9a77c033043ba875f38d5e2eede7
-size 1164
+oid sha256:6d4b4da8791d414723440896d6fc14af6644811878840ebe3dc55ed84ecd5a78
+size 1147
--- cdpr/DataEngine/EntryAggregationEngine.cs
+++ cdpr/DataEngine/EntryAggregationEngine.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b6db9bdf3c4df31d7fe84745c11834b6dd451af19ba35d3cf1a7feda936c399
-size 80
+oid sha256:38dc2c732e6b2f414869f43f443accba6d2b57556b5c78b147d57119b69a6cd0
+size 1102
