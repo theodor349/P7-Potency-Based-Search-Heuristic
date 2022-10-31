558fec8674f90c26619c6b44aabb8a0bf182e5e7
Theodor Risager <theodor349@gmail.com>
2022-10-31 14:03:45 +0100

    CSV Engine

--- cdpr/ClusterDataParser/Program.cs
+++ cdpr/ClusterDataParser/Program.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce7b99e9ef78259c7d2082f112f7d4167a8860912f7fe0d92e1af46eead88f7a
-size 1814
+oid sha256:096653777084eb6bfc2b8e4ca05fd293bf81a70f759b88701cc8e7e159d13406
+size 1998
---
+++ cdpr/DataEngine/CsvEngine.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:145e37e333d5b6ad85bf02ea8a592a7fdff7ec2fae45edc948e782039f0834d7
+size 1256
