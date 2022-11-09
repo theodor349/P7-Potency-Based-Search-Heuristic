48aaa01c91c6fead1d4b73097705006d1ccfc979
Theodor Risager <theodor349@gmail.com>
2022-11-09 09:16:27 +0100

    More columns

--- cdpr/ClusterDataParser/Program.cs
+++ cdpr/ClusterDataParser/Program.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d505d39d9dca109307c003aaa6b1e097d73dc55252dbf118d7656949226f12f
-size 2459
+oid sha256:f5559868bd2c9642f1b478265387db8feca62db6be7da126c83b3474a3c03181
+size 2461
