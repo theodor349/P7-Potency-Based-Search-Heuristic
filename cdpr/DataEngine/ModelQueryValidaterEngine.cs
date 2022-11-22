84b933ddc6d2f06ae828a94ef2539411856efeea
Theodor Risager <theodor349@gmail.com>
2022-11-22 09:51:45 +0100

    Truth Debugging

--- cdpr/DataEngine/ModelQueryValidaterEngine.cs
+++ cdpr/DataEngine/ModelQueryValidaterEngine.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c3ab8143878f4a29837322cfbcc0a2bd8b2437569ef86b75b948eae35caf78e
-size 2548
+oid sha256:7068f224c6786b331e968924f6dd06be8a1679cbb237ef3507ec03a9cec0e0f2
+size 2834
