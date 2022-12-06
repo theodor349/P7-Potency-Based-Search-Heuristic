adeb198bd95a529f172b0f69eda214f7e9551d22
Theodor Risager <theodor349@gmail.com>
2022-12-06 11:40:33 +0100

    Error Stats

--- DataCruncher/Tabels/StatsGenerator.cs
+++ DataCruncher/Tabels/StatsGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd5a0d71268a0d49a043f13b9198b75dd8c87236788b1997ff6a8904dcd9da3a
-size 5512
+oid sha256:21a41d3413647b2352e75a8316bbc4acd2047d11ca4f79b14179210db44e49ef
+size 6131
--- Models/StatsEntry.cs
+++ Models/StatsEntry.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d7bc182b9bc7827fd0811a8ec9fa40661706b0ad1af9d134796259463d8718b
-size 921
+oid sha256:fea8b400aa817a8a92dc4c93871ac9aef6977c02c2ce71397c6ad8515b4c5369
+size 1008
