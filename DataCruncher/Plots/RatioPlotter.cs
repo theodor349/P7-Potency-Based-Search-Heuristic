78acf90f750e759352d62118895dddd7946f17d1
Theodor Risager <theodor349@gmail.com>
2022-12-15 15:19:04 +0100

    Done

--- DataCruncher/Plots/RatioPlotter.cs
+++ DataCruncher/Plots/RatioPlotter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9e55d1976742504cfcded9d5bf64db6dd923d1c22d72fe01a002fe1f31370f1
-size 4616
+oid sha256:b9f874c1879bdc7a4f0de17f71831d416add52dd7bbd5ac62a6afb73fde8e39d
+size 4580
---
+++ DataCruncher/Sections/Appendix_A.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:950692792a596439adcc18cfe9cdda859f46ee7dd2f55e1901b4558cba9f44ca
+size 3447
